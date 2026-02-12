5a7f59f6aaad0709facd6c7d5f2e681c339cd36f
NaseeraVP <naseeravp38@gmail.com>
2026-02-12 16:12:00 +0530

    fix: populate Parent Warehouses table with all companies when enabled

--- eseller_suite/eseller_suite/doctype/eseller_settings/eseller_settings.js
+++ eseller_suite/eseller_suite/doctype/eseller_settings/eseller_settings.js
@@ -5,12 +5,26 @@ frappe.ui.form.on("eSeller Settings", {
 	refresh(frm) {
 		set_parent_warehouse_query(frm);
 	},
+
 	onload(frm) {
 		set_parent_warehouse_query(frm);
+	},
+
+	allow_missing_warehouse_creation(frm) {
+		if (frm.doc.allow_missing_warehouse_creation) {
+			populate_parent_warehouses(frm);
+		} else {
+			clear_parent_warehouses(frm);
+		}
 	}
 });
 
+/**
+ * Apply filter for parent warehouse based on company and is_group
+ */
 function set_parent_warehouse_query(frm) {
+	if (!frm.fields_dict.parent_warehouses) return;
+
 	frm.fields_dict.parent_warehouses.grid.get_field("default_parent_warehouse").get_query =
 		function (doc, cdt, cdn) {
 			let row = locals[cdt][cdn];
@@ -21,4 +35,33 @@ function set_parent_warehouse_query(frm) {
 				}
 			};
 		};
+}
+
+/**
+ * Populate parent warehouses via server call Clears parent warehouses when checkbox is unchecked
+ */
+function populate_parent_warehouses(frm) {
+	frappe.call({
+		method: "eseller_suite.eseller_suite.doctype.eseller_settings.eseller_settings.get_all_companies",
+		callback: function (r) {
+			if (r.message) {
+				frm.clear_table("parent_warehouses");
+
+				r.message.forEach(comp => {
+					let row = frm.add_child("parent_warehouses");
+					row.company = comp.name;
+				});
+
+				frm.refresh_field("parent_warehouses");
+			}
+		}
+	});
+}
+
+/*
+ * Clear table when checkbox unchecked
+ */
+function clear_parent_warehouses(frm) {
+	frm.clear_table("parent_warehouses");
+	frm.refresh_field("parent_warehouses");
 }
--- eseller_suite/eseller_suite/doctype/eseller_settings/eseller_settings.py
+++ eseller_suite/eseller_suite/doctype/eseller_settings/eseller_settings.py
@@ -1,9 +1,17 @@
 # Copyright (c) 2024, efeone and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
-
 
 class eSellerSettings(Document):
 	pass
+
+
+@frappe.whitelist()
+def get_all_companies():
+	"""
+	Returns all Companies for use in eSeller Settings child table population.
+	No client-side DB access is used.
+	"""
+	return frappe.get_all("Company", fields=["name"], order_by="name asc")
