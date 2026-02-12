59b4e71ecc6bf4849a11ec9a600c08d7e8235270
anjusha <anjushasuresh85@gmail.com>
2026-02-12 12:06:43 +0530

    chore: cleanup the code

--- eseller_suite/eseller_suite/doctype/amazon_stn_entry/amazon_stn_entry.js
+++ eseller_suite/eseller_suite/doctype/amazon_stn_entry/amazon_stn_entry.js
@@ -2,9 +2,6 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on("Amazon STN Entry", {
-	onload(frm) {
-		frm.set_df_property('stn_entries', 'cannot_add_rows', true)
-	},
 	stn_file(frm) {
 		if (!frm.doc.stn_file) {
 			frm.clear_table("stn_entries");
