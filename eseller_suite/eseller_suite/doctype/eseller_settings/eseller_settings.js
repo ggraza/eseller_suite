39e93defb4551f8fdcfb0515afeaeff09c7179e6
NaseeraVP <naseeravp38@gmail.com>
2026-02-12 11:21:27 +0530

    feat:Customize eSeller Settings DocType

--- eseller_suite/eseller_suite/doctype/eseller_settings/eseller_settings.js
+++ eseller_suite/eseller_suite/doctype/eseller_settings/eseller_settings.js
@@ -1,8 +1,24 @@
 // Copyright (c) 2024, efeone and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("eSeller Settings", {
-// 	refresh(frm) {
+frappe.ui.form.on("eSeller Settings", {
+	refresh(frm) {
+		set_parent_warehouse_query(frm);
+	},
+	onload(frm) {
+		set_parent_warehouse_query(frm);
+	}
+});
 
-// 	},
-// });
+function set_parent_warehouse_query(frm) {
+	frm.fields_dict.parent_warehouses.grid.get_field("default_parent_warehouse").get_query =
+		function (doc, cdt, cdn) {
+			let row = locals[cdt][cdn];
+			return {
+				filters: {
+					company: row.company || "",
+					is_group: 1
+				}
+			};
+		};
+}
