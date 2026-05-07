ee055c2f2d347d9ecb044d7950f85ec7ec510ffb
Sherin KR <sherinkrply@gmail.com>
2026-05-07 13:56:05 +0530

    refactor: failed invoice record optimisation

--- eseller_suite/eseller_suite/doctype/amazon_failed_invoice_record/amazon_failed_invoice_record.py
+++ eseller_suite/eseller_suite/doctype/amazon_failed_invoice_record/amazon_failed_invoice_record.py
@@ -3,20 +3,18 @@
 
 import frappe
 from frappe.model.document import Document
-
-
 class AmazonFailedInvoiceRecord(Document):
 	def after_insert(self):
-		self.delete_old_failed_invoice_record_if_any()
+		self.delete_old_failed_invoice_records()
 
-	def delete_old_failed_invoice_record_if_any(self):
-		existing_failed_invoice_record = frappe.db.exists(
+	def delete_old_failed_invoice_records(self):
+		if not self.invoice_id:
+			return
+
+		frappe.db.delete(
 			"Amazon Failed Invoice Record",
 			{
 				"invoice_id": self.invoice_id,
 				"name": ["!=", self.name]
 			}
 		)
-
-		if existing_failed_invoice_record:
-			frappe.delete_doc("Amazon Failed Invoice Record", existing_failed_invoice_record, ignore_permissions=True, force=True)
